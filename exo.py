52979d623cea223e46e1b1c413a783a65b6fbe41
Minipotal <Msylvestre52600@gmail.com>
2022-11-08 17:26:40 +0100

    nouveaux exos

--- exo.py
+++ exo.py
@@ -39,24 +39,54 @@ y=float(input("Entrez un nombre Y : "))
 
 #add(x,y)
 def additionnerXplusY(x,y):
-    print(x+y)
+    return(x+y)
 #sub(x,y)
 def soustraireXavecY(x,y):
-    print(x-y)
+    return(x-y)
 #multiply(x,y)
 def multiplierXparY(x,y):
-    print(x*y)
+    return(x*y)
 #divide(x,y)
 def diviserXparY(x,y):
-    print(x/y)
+    if (y==0):
+        print("Division par 0 impossible")
+    else:    
+        return(x/y)
 #modulo(x,y)
 def modulerXparY(x,y):
-    print(x%y)
+    return(x%y)
 #SalaireNet(Brut, coefficient)
 def salaireNet(SalaireBrut, Coefficient):
-    print(SalaireBrut*Coefficient)
+    return(SalaireBrut*Coefficient)
 #SalaireParSeconde(salaire horaire, heure par jour ouvré, nb jours ouvré par an)
 def salaireParSeconde(salaireHoraire, HeureOuvre, joursOuvres):
+    #Calcul du salaire annuel
     salaireAnnuel=salaireHoraire*HeureOuvre*joursOuvres
+    #Calcul du temps en seconde annuel
     tempsAnnuel=31536000
-    print(salaireAnnuel/tempsAnnuel)
+    #Calcul du salaire par seconde
+    return(salaireAnnuel/tempsAnnuel)
+    
+
+
+#Définir une fonction withdrawFees qui retire un pourcentage a un totoal en fonction d'un total et d'un pourcentage
+    def withdrawFees(total, percent):
+    #definir fees en fonction d'un total et d'un pourcentage
+        fees = total * (percent / 100)
+     #soustraire fees au total
+        result = total - fees
+     #retourner la valeur obtenue
+        return result
+
+    #Definir une fonction qui retourne le salaire net en fonction du salaire brut (float) et du secteur d'activité (isPublic > boolean)
+    def calculBrutEnNet(salaireBrut, isPublic):
+        #Si je suis de la fonction publique
+        if isPublic:
+            #Alors je soustrais 15% de mon salaire brut
+            salaireNet = withdrawFees(salaireBrut,15)
+        #Sinon je suis un travailleur du secteur privé
+        else:
+            #Alors je soustrais 23% a mon salaire brut
+            salaireNet = withdrawFees(salaireBrut,23)
+        #retourner salaireNet
+        return salaireNet
