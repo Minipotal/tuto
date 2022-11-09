69f9c55bb8bdac4e26e4c99b2b5c8041559e3218
Minipotal <Msylvestre52600@gmail.com>
2022-11-09 10:03:52 +0100

    exos + suite fibo

--- exo2.py
+++ exo2.py
@@ -1,3 +1,4 @@
+"""
 #def input():
     # Exercice
     # Faire un mini jeu qui affiche un message lorsque input envoie le bon caractère
@@ -29,18 +30,95 @@ else:
 compteur = 0
 letter=" "
 # Fin
-
+"""
 
 #Exercice 1
 #Faire une fonction qui concatene 2 chaines de caractères, en les séparant par une virgule
+
+#Debut
+
+#Entrer un premier mot
+str1=(input("Entrez votre premier mot : "))
+#Entrer un deuxieme mot
+str2=(input("Entrez votre deuxieme mot : "))
+#concatener les deux mot avec une separation virgule
+print(str1,str2,sep=",")
+
+#Fin
+
+#ou alors : exercice 1
+
+def concatWithComma(strA, strB):
+    #on s'assure que strA est bien un string
+    stringifiedA = str(strA)
+    #on s'assure que strB est bien un string
+    stringifiedb = str(strB)
+    #Retourne strA + ',' + strB
+    return strA + ", " + strB
+
+concatWithComma(23, "toto") #Retourne "23, toto"
+
+
 
 #Exercice 2
 #Faire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de caractère
 #Avec l'ensemble des occurences d'un chiffre e.g. :
 #Pour tableau = [0,1,1,1,0,1,1,0,1]
 #La fonction(tableau, 0) doit renvoyer "0,4,7", n'hésitez pas à implémenter la première fonction
+tableau = [0,1,1,1,0,1,1,0,1]
 
-#Exercice 3
-#Renvoyer / afficher un message
-msg = str(input("Entrez votre lettre : "))
+#Definir la fonction fidIndex qui itere sur le tableau, cherchant l'index
+#des differentes occurences de x
+def findIndex(tableau, x):
+    #Definir i un index de départ
+    i=0
+    #Definir chaineRetour telle qu'une chaine de caractere vide
+    chaineRetour = ''
+    #Je defini un boolean tel que firstTurn est true
+    firstTurn = "True"
+    #Tant que i est different du nombre d'elt dans le tableau
+    while i!= len(tableau):
+        #Alors j'attribue a une variable la valeur de tableau a l'index i
+        selected = tableau[i]
+        #Si selected est egale a x ET que firstTurn est true
+        if(selected==x & firstTurn=="True"):
+            #Alors on assigne a chaineRetour le retour de str(i)
+
+            #Changer la valeur de firstTurn a false
+            firstTurn = "False"
+        #Sinon si selected egal a x
+        elif(selected ==x):
+            #Alors j'assigne le retour de concatWithComma tel que : concatWithComma(chaineRetour, i) a chaineRetour
+            chaineRetour = concatWithComma(chaineRetour, i)
+        #j'incremente i de 1
+        i=i+1
+#Definir mon index 
+    return chaineRetour
+
+#Exercice 3 
+
+#Debut
+
+#Ecrire un message
+msg = str(input("Entrez votre message : "))
+#Afficher le message
 print (msg)
+
+#Fin
+
+
+# Programme pour générer la suite de Fibonacci en utilisant la récursivité
+def fibonacci(n):
+    #Si n inferieur a 1, pas de suite possible
+    if(n <= 1):
+        #Retourner n
+        return n
+    #Sinon calculer la suite de fibonacci
+    else:
+        return (fibonacci(n-1) + fibonacci(n-2))
+#definir combien de passage
+n = int(input("Entrez le nombre de termes:"))
+#Afficher la suite de fibonacci
+print("Suite de Fibonacci en utilisant la recursion :")
+for i in range(n):
+    print(fibonacci(i))
